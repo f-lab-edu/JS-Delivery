98f97ac64710c25aebae416928ccf4772dc03840
jiiinang <chjh2425@naver.com>
2022-08-05 21:26:40 +0900

    Initial commit

---
+++ src/main/java/com/flab/jsdelivery/JsdeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.flab.jsdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsdeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flab/jsdelivery/JsdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.jsdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
